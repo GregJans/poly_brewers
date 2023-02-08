0d3a3e189881f937720fa4705eb114cdddda49f3
Greg <gtj0806@gmail.com>
2023-02-07 19:43:46 -0500

    Added sign-in page under profile page * will eventually only show if user is not logged in * no buttons have functionality currenty * google logo icon needs to be cropped

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,11 +1,288 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
+  State<ProfilePage> createState() => ProfilePageState();
+}
+
+class ProfilePageState extends State<ProfilePage> {
+  bool passwordVisibility = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Visibility(
+          /*visible: responsiveVisibility(
+            context: context,
+            tablet: false,
+          ),*/
+          child: Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(24, 24, 24, 24),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+                  child: Text(
+                    'Want more from Poly Brewers?',
+                    style: TextStyle(
+                          //fontFamily: 'Roboto',
+                          fontSize: 50,
+                        ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 50),
+                  child: Text(
+                    'Sign in or create an account to unlock all functionality',
+                    style: TextStyle(
+                          //fontFamily: 'Poppins',
+                          fontSize: 25,
+                        ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                  child: Container(
+                    width: 500,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 6,
+                          color: Color(0x3416202A),
+                          offset: Offset(0, 2),
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(2, 2, 2, 2),
+                      child: TextFormField(
+                        //controller: textController1,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          labelText: 'Your email address',
+                          //labelStyle: FlutterFlowTheme.of(context).bodyText2,
+                          //hintStyle: FlutterFlowTheme.of(context).bodyText2,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding: const EdgeInsetsDirectional.fromSTEB(20, 24, 20, 24),
+                        ),
+                        //style: FlutterFlowTheme.of(context).bodyText1,
+                        maxLines: null,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
+                  child: Container(
+                    width: 500,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 6,
+                          color: Color(0x3416202A),
+                          offset: Offset(0, 2),
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(2, 2, 2, 2),
+                      child: TextFormField(
+                        //controller: textController2,
+                        obscureText: !passwordVisibility,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          //labelStyle: FlutterFlowTheme.of(context).bodyText2,
+                          //hintStyle: FlutterFlowTheme.of(context).bodyText2,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const  BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          contentPadding: const EdgeInsetsDirectional.fromSTEB(20, 24, 20, 24),
+                          suffixIcon: InkWell(
+                            onTap: () => setState(
+                              () => passwordVisibility = !passwordVisibility,
+                            ),
+                            focusNode: FocusNode(skipTraversal: true),
+                            child: Icon(
+                              passwordVisibility
+                                  ? Icons.visibility_outlined
+                                  : Icons.visibility_off_outlined,
+                              color: const Color(0xFF757575),
+                              size: 22,
+                            ),
+                          ),
+                        ),
+                        //style: FlutterFlowTheme.of(context).bodyText1,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              //color: FlutterFlowTheme.of(context).primaryBackground,
+                            ),
+                            alignment: const AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 16, 0),
+                              child: TextButton(
+                                onPressed: (){
+                                  debugPrint("Sign in");
+                                },
+                                child: const Text(
+                                  'Sign In',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              //color: FlutterFlowTheme.of(context).primaryBackground,
+                            ),
+                            alignment: const AlignmentDirectional(-1, 0),
+                            child: Padding(
+                              padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+                              child: TextButton(
+                                onPressed: () {
+                                  debugPrint("Sign Up");
+                                },
+                                child: const Text(
+                                  'Sign Up',
+                                  style: TextStyle(
+                                    //fontFamily: 'Poppins',
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                              child: IconButton(
+                                icon: Image.asset(
+                                  'google_logo.png',
+                                  fit:BoxFit.fill,
+                                ),
+
+                                onPressed: () async {
+                                  debugPrint("Sending to google log in");
+                                },
+                              
+                                 
+                              //borderColor: Colors.transparent,
+                              //borderRadius: 30,
+                              //borderWidth: 1,
+                              //buttonSize: 50,
+                              //fillColor: FlutterFlowTheme.of(context).secondaryBackground,
+                              
+                                    
+                                //color: Colors.red,
+                                //size: 24,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
